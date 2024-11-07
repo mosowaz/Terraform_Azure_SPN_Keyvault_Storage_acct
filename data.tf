6f808c93bf507531e2af56cf2729049211277b39
Moses <150498910+mosowaz@users.noreply.github.com>
2024-11-07 13:29:37 -0500

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,5 +1,9 @@
 data "azuread_client_config" "current" {}
 
+data "azuread_service_principal" "spn" {
+  object_id = azuread_service_principal.spn.object_id
+}
+
 data "azurerm_subscription" "primary" {}
 
 data "azurerm_client_config" "current" {}
